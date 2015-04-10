6fc0dcb44db20e6ce1300acebbf635d80ba0b161
rsfitz5 <roryf@princeton.edu>
2015-04-10 15:42:50 -0400

    [views.py] minor changes

--- charterclub/views.py
+++ charterclub/views.py
@@ -3,24 +3,26 @@ from django import template
 from django.shortcuts import render
 
 def index(request):
-    html = "Hello World"
-    return render(request, "index.html")
+   html = "Hello World"
+   return render(request, "index.html")
 #    return HttpResponse(html)
 
 def calendar(request):
-    return "This is a completely functional calendar"
+   return render(request, "calendar.html")
+   # return HttpResponse("This is a completely functional calendar")
 
 def menu(request):
-    return "This is a completely functional menu"
+   return HttpResponse("This is a completely functional menu")
 
 def history(request):
-    return "This is a completely functional history"
+   return HttpResponse("This is a completely functional history")
 
 def song(request):
-    return "This is a completely functional song"
+   return HttpResponse("This is a completely functional song")
 
 def constitution(request):
-    return "This is a completely functional constitution"
+   return HttpResponse("This is a completely functional constitution")
 
 def underconstruction(request):
-    return "This feature is not functional at all"
+   return HttpResponse("This is a under construction!")
+
