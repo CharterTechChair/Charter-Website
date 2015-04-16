6b78525c162c12ea7c8908c32f19b767a81bbb05
Jeremy Whitton <jdwhitton@yahoo.com>
2015-04-15 20:55:28 -0400

    faceboard now has more features - distinguishes between class years. still not pretty

--- charterclub/views.py
+++ charterclub/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from django.template import RequestContext
 from django.template.loader import get_template
 from django.conf import settings
-from os import listdir
+from datetime import date
+from os import listdir, path
 
 def index(request):
    html = "Hello World"
@@ -15,10 +16,28 @@ def calendar(request):
    return render(request, "calendar.html")
    # return HttpResponse("This is a completely functional calendar")
 
+
+   
 def faceboard(request):
+   def picsfromyear(year):
+      piclocation = "static/img/faceboard/"
+      pics = []
+      if not path.exists(piclocation + str(year)):
+         return
+      
+      for pic in listdir(piclocation + str(year)):
+         pics.append(str(year) + "/" + pic)
+      return pics
+            
+   year = date.today().year
    template = get_template("faceboard.html")
+   seniorpics = picsfromyear(year)
+   juniorpics = picsfromyear(year + 1)
+   sophpics = picsfromyear(year + 2)
    context = RequestContext(request,
-                            {"pics" : listdir("static/img/faceboard")})
+                            {"seniorpics" : seniorpics,
+                             "juniorpics" : juniorpics,
+                             "sophpics" : sophpics})
    return HttpResponse(template.render(context))
    
 def menu(request):
