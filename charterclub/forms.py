adcc792240e94d1fc902be719766422b42d00f16
rsfitz5 <roryf@princeton.edu>
2015-04-29 11:34:02 -0400

    fixed forms

--- charterclub/forms.py
+++ charterclub/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field
-from crispy_forms.bootstrap import (
-    PrependedText, PrependedAppendedText, FormActions)
+# from crispy_forms.helper import FormHelper
+# from crispy_forms.layout import Submit, Layout, Field
+# from crispy_forms.bootstrap import (
+#     PrependedText, PrependedAppendedText, FormActions)
 
 class FeedbackForm(forms.Form):
-   helper = FormHelper()
+   # helper = FormHelper()
    anonymous_feedback  = forms.CharField(
       widget=forms.Textarea, 
       label="Anonymous Feedback",
       required=False)
-   helper.add_input(Submit('submit', 'submit', css_class='btn-primary'))
+   # helper.add_input(Submit('submit', 'submit', css_class='btn-primary'))
--- charterclub/settings.py
+++ charterclub/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
     '.princetoncharterclub.org',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+# CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # Application definition
 
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'charterclub',
-    'crispy_forms',
+    # 'crispy_forms',
 )
 
 MIDDLEWARE_CLASSES = (
