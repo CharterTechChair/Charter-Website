bfb78e115c46fd633046464aff61ac9b2e579c25
rsfitz5 <roryf@princeton.edu>
2015-05-10 16:57:08 -0400

    restructure base.html

--- charterclub/settings.py
+++ charterclub/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 # if this is true, get_username(request) will return testuser. use for
 # testing when CAS is not available
-CAS_DISABLED = False
+CAS_DISABLED = True
 
 TEMPLATE_DEBUG = True
 
