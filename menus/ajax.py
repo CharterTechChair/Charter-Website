f469302add243f06ad7e4c7dc40f5be66a837ebe
Jeremy Whitton <jdwhitton@yahoo.com>
2015-05-11 14:31:35 -0400

    ajax now working for menu entry

--- menus/ajax.py
+++ menus/ajax.py
@@ -1,10 +1,18 @@
 from dajax.core import Dajax
 from dajaxice.decorators import dajaxice_register
+from menus.models import MenuItem
 
 @dajaxice_register
 def updatemenu(request, date):
     dajax = Dajax()
 
-    dajax.assign("#id_lunch_food", "innerHTML", str(date))
+    menu = MenuItem.objects.filter(date=date)
+    if len(menu) > 0:
+        menu = menu[0]
+    else:
+        menu = MenuItem(date = date, lunch_food = "", dinner_food = "")
+
+    dajax.assign("#id_lunch_food", "innerHTML", menu.lunch_food)
+    dajax.assign("#id_dinner_food", "innerHTML", menu.dinner_food)
 
     return dajax.json()
