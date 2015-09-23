e963e9b420119ca10cb880833a39572c28be3194
theleastinterestingcoder <quanzhou64@gmail.com>
2015-09-22 22:07:34 -0400

    added whitenoise==2.0.4 to requirements.txt

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,5 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+application = get_wsgi_application()        # From https://warehouse.python.org/project/whitenoise/
+application = DjangoWhiteNoise(application) # From https://warehouse.python.org/project/whitenoise/
