86afd278712c680220cf1fb784c3735fd3f48e0a
thomasrr <thomasrr@princeton.edu>
2017-10-04 21:16:57 -0400

    other migrations

---
+++ recruitment/migrations/0013_auto_20171004_2104.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recruitment', '0012_auto_20171002_1048'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='prospectiveevententry',
+            name='signup_date',
+            field=models.DateField(default=datetime.date(2017, 10, 5), blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='prospectivemealentry',
+            name='signup_date',
+            field=models.DateField(default=datetime.date(2017, 10, 5), blank=True),
+            preserve_default=True,
+        ),
+    ]
